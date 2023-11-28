9e1c9a2b8e36e7e24e3233a102981440038541cf
AniketRana07 <aniket77rana@gmail.com>
2023-11-28 14:20:18 +0530

    First Commit

---
+++ src/app/lib/initUser.tsx
@@ -0,0 +1,26 @@
+import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+import { db } from "@/app/lib/database"
+
+export const initUser = async() => {
+    const user = await currentUser()
+
+    if(!user) return redirectToSignIn()
+
+    const profile = await db.user.findUnique({
+        where:{
+            userId:user.id,
+        },
+    })
+
+    if(profile) return profile
+    const newProfile = await db.user.create({
+        data:{
+            userId: user.id,
+            name: `${user.firstName} ${user.lastName}`,
+            imageUrl: user.imageUrl,
+            email: user.emailAddresses[0].emailAddress
+        }
+    }) 
+
+    return newProfile
+}
